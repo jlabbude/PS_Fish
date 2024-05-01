970abf4a77ef6b6797b4b627574dbf46391e4b7f
Lucas <jlabbude@hotmail.com>
2024-05-01 15:56:43 -0300

    initial commit

---
+++ build/CMakeFiles/robloxteste.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/robloxteste.dir/main.cpp.obj"
+  "CMakeFiles/robloxteste.dir/main.cpp.obj.d"
+  "librobloxteste.dll.a"
+  "robloxteste.exe"
+  "robloxteste.exe.manifest"
+  "robloxteste.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/robloxteste.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
